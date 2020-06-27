cb1413037eaf66425d92911a7f97b712a34856dd
Fatih Ceylan <fceylan001@gmail.com>
2020-06-27 22:23:46 +0300

    async update

---
+++ Async and Await/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("C:\\Users\\feti\\source\\repos\\Async and Await\\New Text Document.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            lblCount.Text = "Processing file. please wait...";
+            int count = await task;
+            lblCount.Text = count.ToString() + " characters in the file";
+        }
+    }
+}
